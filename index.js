2fe23937ef3d9882666419c23f3bef6a329fc3ae
josheriksen <joshua.d.eriksen@gmail.com>
2018-01-16 19:04:20 -1000

    add static files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const PORT = process.env.PORT || 8080
 
 express ()
+.use(express.static(path.join(__dirname)))
     .get('/', (req, res) => res.sendFile(path.join(__dirname+'/webpage.html')))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
